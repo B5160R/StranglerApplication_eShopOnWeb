a6be747c6a1d02056b95a42ce65a9ed2f00023cf
B5160R <freidrichson@gmail.com>
2024-09-08 12:59:47 +0200

    updated Basket Api and added gitignore

--- Basket/Basket.Api/Program.cs
+++ Basket/Basket.Api/Program.cs
@@ -1,3 +1,5 @@
+using Basket.Api.Endpoints;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,74 +18,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-// Basket services 
- 
-app.MapPost("/AddItemToBasket", (string username, int catalogItemId, decimal price, int quantity) =>
-{
-    return $"AddItemToBasket called with username: {username}, catalogItemId: {catalogItemId}, price: {price}, quantity: {quantity}";
-})
-.WithName("AddItemToBasket")
-.WithOpenApi()
-.WithTags("Basket Services");
-
-app.MapDelete("/DeleteBasketAsync", (int basketId) =>
-{
-    return $"DeleteBasketAsync called with basketId: {basketId}";
-})
-.WithName("DeleteBasketAsync")
-.WithOpenApi()
-.WithTags("Basket Services");
-
-app.MapPut("/SetQuantities", (int basketId, Dictionary<string, int> quantities) =>
-{
-    return $"SetQuantities called with basketId: {basketId}, quantities: {quantities}";
-})
-.WithName("SetQuantities")
-.WithOpenApi()
-.WithTags("Basket Services");
-
-app.MapGet("/TransferBasketAsync", (string anonymousId, string userName) =>
-{
-    return $"TransferBasketAsync called with anonymousId: {anonymousId}, userName: {userName}";
-})
-.WithName("TransferBasketAsync")
-.WithOpenApi()
-.WithTags("Basket Services");
-
-// Basket View Model services
-
-app.MapGet("/GetOrCreateBasketForUser", (string userName) =>
-{
-    return $"GetOrCreateBasketForUser called with userName: {userName}";
-})
-.WithName("GetOrCreateBasketForUser")
-.WithOpenApi()
-.WithTags("Basket View Model Services");
-
-
-app.MapPost("/CreateBasketForUser", (string userId) =>
-{
-    return $"CreateBasketForUser called with userId: {userId}";
-})
-.WithName("CreateBasketForUser")
-.WithOpenApi()
-.WithTags("Basket View Model Services");
-
-// string == BasketItem
-app.MapGet("/GetBasketItems", (IReadOnlyCollection<string> basketItems) =>
-{
-    return $"GetBasketItems called with basketItems: {basketItems}";
-})
-.WithName("GetBasketItems")
-.WithOpenApi()
-.WithTags("Basket View Model Services");
-
-app.MapGet("/CountTotalBasketItems", (string username) =>
-{
-    return $"CountTotalBasketItems called with username: {username}";
-})
-.WithName("CountTotalBasketItems")
-.WithOpenApi()
-.WithTags("Basket View Model Services");
+app.MapBasketEndpoints();
 
 app.Run();
