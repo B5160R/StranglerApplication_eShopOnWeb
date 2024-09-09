e12904c9000f1bab4e756acbd939337efb5c3673
Christian Schrøder <csc@unik.dk>
2024-09-09 10:30:33 +0200

    CatalogItemFeature

--- Catalog/Catalog.Api/Program.cs
+++ Catalog/Catalog.Api/Program.cs
@@ -1,7 +1,18 @@
+using Catalog.Infrastructure.Data;
 using FastEndpoints;
 using FastEndpoints.Swagger;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDbContext<CatalogContext>(c =>
+{
+  var connectionString = builder.Configuration[builder.Configuration["AZURE_SQL_CATALOG_CONNECTION_STRING_KEY"] ?? ""];
+  c.UseSqlServer(connectionString, sqlOptions => sqlOptions.EnableRetryOnFailure());
+});
+
+
 builder.Services.AddFastEndpoints();
 builder.Services.SwaggerDocument(); 
 
---
+++ Catalog/Catalog.Core/Specifications/CatalogItemsSpecification.cs
@@ -0,0 +1,18 @@
+﻿using Catalog.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ardalis.Specification;
+
+namespace Catalog.Core.Specifications
+{
+  public class CatalogItemsSpecification : Specification<CatalogItem>
+  {
+    public CatalogItemsSpecification(params int[] ids)
+    {
+      Query.Where(c => ids.Contains(c.Id));
+    }
+  }
+}
